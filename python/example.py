931a052d8d1f870856b06b0557fc8f441a89b00c
Munzir Zafar <mzafar7@gatech.edu>
2018-12-08 22:21:58 -0500

    added two features: external timestepping and resetting. working. just having a issue when relaunching 41 and 44. a bunch of couts are littered therefore, because debugging. the issue needs to be resolved and cleanup needs to be done

--- python/example.py
+++ python/example.py
@@ -14,6 +14,7 @@ def str(xvec):
 
 
 interface_context = pykrang.InterfaceContext('pykrang')
+world = pykrang.WorldInterface(interface_context, 'sim-cmd')
 imu = pykrang.FloatingBaseStateSensorInterface(interface_context, 'imu-data')
 wheels = pykrang.MotorInterface(interface_context, 'wheels', 'amc-cmd',
                                 'amc-state', 2)
@@ -27,48 +28,47 @@ right_arm = pykrang.MotorInterface(interface_context, 'right-arm', 'rlwa-cmd',
                                    'rlwa-state', 7)
 right_arm.LockCommand()
 
-t = 0.0
+iter = 0
+dt = 0.001
 while True:
     try:
-        imu.UpdateState()
-        print 'imu angle: ', "%.4f" % (imu.base_angle * 180.0 / pi)
-        print 'imu speed: ', "%.4f" % imu.base_angular_speed
-
-        wheels.UpdateState()
-        print 'wheel pos: ', str(wheels.GetPosition())
-        print 'wheel vel: ', str(wheels.GetVelocity())
-        print 'wheel cur: ', str(wheels.GetCurrent())
-
-        torso.UpdateState()
-        print 'torso pos: ', str(torso.GetPosition())
-        print 'torso vel: ', str(torso.GetVelocity())
-        print 'torso cur: ', str(torso.GetCurrent())
-
-        left_arm.UpdateState()
-        print 'L-arm pos: ', str(left_arm.GetPosition())
-        print 'L-arm vel: ', str(left_arm.GetVelocity())
-        print 'L-arm cur: ', str(left_arm.GetCurrent())
-
-        right_arm.UpdateState()
-        print 'R-arm pos: ', str(right_arm.GetPosition())
-        print 'R-arm vel: ', str(right_arm.GetVelocity())
-        print 'R-arm cur: ', str(right_arm.GetCurrent())
-
         current_command = pykrang.VectorOfDoubles()
         period = 5.0  # sec
         current_command[:] = [
-            30 * sin(2 * pi * t / period), 30 * sin(2 * pi * t / period)
+            30 * sin(2 * pi * iter * dt / period),
+            30 * sin(2 * pi * iter * dt / period)
         ]
         wheels.CurrentCommand(current_command)
 
-        time.sleep(0.5)
-        t += 0.5
+        world.Step()
 
+        iter = iter + 1
+
+        if iter % 500 == 0:
+            #imu.UpdateState()
+            #print 'imu angle: ', "%.4f" % (imu.base_angle * 180.0 / pi)
+            #print 'imu speed: ', "%.4f" % imu.base_angular_speed
+
+            wheels.UpdateState()
+            print 'wheel pos: ', str(wheels.GetPosition())
+            print 'wheel vel: ', str(wheels.GetVelocity())
+            print 'wheel cur: ', str(wheels.GetCurrent())
+
+            torso.UpdateState()
+            print 'torso pos: ', str(torso.GetPosition())
+            print 'torso vel: ', str(torso.GetVelocity())
+            print 'torso cur: ', str(torso.GetCurrent())
+
+            left_arm.UpdateState()
+            print 'L-arm pos: ', str(left_arm.GetPosition())
+            print 'L-arm vel: ', str(left_arm.GetVelocity())
+            print 'L-arm cur: ', str(left_arm.GetCurrent())
+
+            right_arm.UpdateState()
+            print 'R-arm pos: ', str(right_arm.GetPosition())
+            print 'R-arm vel: ', str(right_arm.GetVelocity())
+            print 'R-arm cur: ', str(right_arm.GetCurrent())
+
+        interface_context.Run()
     except KeyboardInterrupt:
-        imu.Destroy()
-        wheels.Destroy()
-        torso.Destroy()
-        left_arm.Destroy()
-        right_arm.Destroy()
-        interface_context.Destroy()
         sys.exit(0)
